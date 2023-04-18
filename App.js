06ad0397769dd8ac0b8443da7141d94fb72e3f73
Szymon <Szymon.nowacki124@gmail.com>
2023-04-18 21:35:55 +0200

    to jest test

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Picker } from 'react-native';
+
+const Categories = ['Spożywcze', 'Artykuły Biurowe', 'Rozrywka', 'Jedzenie', 'Sport', 'Ubrania'];
+
 
 export default function App() {
+  const [kwota, setKwota] = useState('');
+  const [data, setData] = useState('');
+  const [kategoria, setKategoria] = useState(Categories[0]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 16 }}>
+      <Text>Kwota:</Text>
+      <TextInput
+        value={kwota}
+        onChangeText={setKwota}
+        keyboardType="numeric"
+        style={{ borderWidth: 1, padding: 8 }}
+      />
+
+      <Text>Data:</Text>
+      <TextInput
+        value={data}
+        onChangeText={setData}
+        keyboardType="numeric"
+        style={{ borderWidth: 1, padding: 8 }}
+      />
+
+      <Text>Kategoria:</Text>
+      <Picker
+        selectedValue={kategoria}
+        onValueChange={setKategoria}
+        style={{ borderWidth: 1 }}
+      >
+        {Categories.map((category) => (
+          <Picker.Item key={category} label={category} value={category} />
+        ))}
+      </Picker>
     </View>
   );
 }
@@ -18,3 +49,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
